a2d2eb312ca63635e1b5f640edba674cefb67d46
Dagmawi Hailu <135310799+Dagmawi-Y@users.noreply.github.com>
2023-11-20 22:11:22 +0300

    Time: 162 ms (5.72%), Space: 49.6 MB (5.22%) - LeetHub

---
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+    var output = [];
+    for (i = left; i <= right; i++){
+        if (SDN(i) === true){
+            output.push(i)
+        }else{
+            console.log('no');
+        }
+    }
+    return output;
+};
+
+function SDN(number) {
+    const numStr = number.toString();
+    
+    for (let i = 0; i < numStr.length; i++) {
+        const digit = parseInt(numStr[i]);
+        
+        if (digit === 0 || number % digit !== 0) {
+            return false;
+        }
+    }
+    
+    return true;
+}
