09fa8fd78ab41be0c573f4bff359796a7ac302e4
Dagmawi Hailu <135310799+Dagmawi-Y@users.noreply.github.com>
2023-11-20 22:48:39 +0300

    Time: 45 ms (85.91%), Space: 42.1 MB (16.99%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.js
+++ 2469-convert-the-temperature/2469-convert-the-temperature.js
@@ -3,12 +3,5 @@
  * @return {number[]}
  */
 var convertTemperature = function(celsius) {
-    var op = [];
-    var k;
-    var f;
-    k = celsius + 273.15;
-    f = (celsius * 1.80) + 32.00;
-    op.push(k);
-    op.push(f);
-    return op;
+    return [celsius + 273.15, celsius * 1.80 + 32.00];
 };
