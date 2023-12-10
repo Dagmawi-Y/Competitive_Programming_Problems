0835fbbef628c3299a5eed924ba60938d310ecaf
Dagmawi Hailu <135310799+Dagmawi-Y@users.noreply.github.com>
2023-12-11 01:36:43 +0300

    Time: 56 ms (76.99%), Space: 43.4 MB (22.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    op = [];
-    for (i = 0; i < nums.length; i++) {
-        for (j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] == target) {
-                op.push(i);
-                op.push(j);
-                return op;
-            }
+    const numMap = new Map();
+    
+    for (let i = 0; i < nums.length; i++) {
+        const complement = target - nums[i];
+        
+        if (numMap.has(complement)) {
+            return [numMap.get(complement), i];
         }
+        
+        numMap.set(nums[i], i);
     }
     
+    return "No two elements sum up to the target";
 };
