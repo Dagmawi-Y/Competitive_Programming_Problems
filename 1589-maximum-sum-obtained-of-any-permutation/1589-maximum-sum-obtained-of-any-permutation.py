0897350136a832fc83043dab62ebd29db0d49678
Dagmawi Hailu <135310799+Dagmawi-Y@users.noreply.github.com>
2024-08-22 22:51:42 +0300

    Time: 1273 ms (43.64%), Space: 47.2 MB (74.55%) - LeetHub

--- 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
+++ 1589-maximum-sum-obtained-of-any-permutation/1589-maximum-sum-obtained-of-any-permutation.py
@@ -9,24 +9,19 @@ class Solution(object):
         n = len(nums)
         freq = [0] * (n + 1)
         
-        # Calculate frequency of each index using a difference array approach
         for start, end in requests:
             freq[start] += 1
             if end + 1 < n:
                 freq[end + 1] -= 1
         
-        # Convert frequency array to actual frequency counts
         for i in range(1, n):
             freq[i] += freq[i - 1]
         
-        # Remove the extra element added for ease of calculation
         freq = freq[:-1]
         
-        # Sort the frequency array and nums array
         freq.sort()
         nums.sort()
         
-        # Compute the maximum sum
         max_sum = 0
         for f, num in zip(freq, nums):
             max_sum = (max_sum + f * num) % MOD
