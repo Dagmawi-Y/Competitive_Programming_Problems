351983086859eab3f50dd14b6a22475a1b1b1d70
Dagmawi Hailu <135310799+Dagmawi-Y@users.noreply.github.com>
2024-08-22 20:44:39 +0300

    Time: 16 ms (35.30%), Space: 11.6 MB (27.25%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -4,6 +4,4 @@ class Solution(object):
         :type celsius: float
         :rtype: List[float]
         """
-        kelvin = celsius + 273.15
-        fahrenheit = celsius * 1.80 + 32.00
-        return [kelvin, fahrenheit]
+        return [celsius + 273.15, celsius * 1.8 + 32.0]
