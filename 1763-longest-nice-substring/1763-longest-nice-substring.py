3d18fdf1944133a12d7f2eafc76138266cbb559d
Dagmawi Hailu <135310799+Dagmawi-Y@users.noreply.github.com>
2024-08-22 22:57:48 +0300

    Time: 340 ms (12.69%), Space: 11.6 MB (89.85%) - LeetHub

--- 1763-longest-nice-substring/1763-longest-nice-substring.py
+++ 1763-longest-nice-substring/1763-longest-nice-substring.py
@@ -5,23 +5,25 @@ class Solution(object):
         :rtype: str
         """
         def is_nice(sub):
-            lower = set()
-            upper = set()
+            lower_seen = set()
+            upper_seen = set()
             for ch in sub:
                 if ch.islower():
-                    lower.add(ch)
-                elif ch.isupper():
-                    upper.add(ch)
-            return all(ch.upper() in upper for ch in lower) and all(ch.lower() in lower for ch in upper)
-
-        max_len = 0
-        longest = ""
-
-        for i in range(len(s)):
-            for j in range(i + 1, len(s) + 1):
-                substr = s[i:j]
-                if is_nice(substr) and len(substr) > max_len:
-                    max_len = len(substr)
-                    longest = substr
-
-        return longest
+                    lower_seen.add(ch)
+                else:
+                    upper_seen.add(ch)
+            return all(ch.upper() in upper_seen for ch in lower_seen) and \
+                   all(ch.lower() in lower_seen for ch in upper_seen)
+        
+        max_length = 0
+        longest_substring = ""
+        n = len(s)
+        
+        for start in range(n):
+            for end in range(start + 1, n + 1):
+                substring = s[start:end]
+                if is_nice(substring) and len(substring) > max_length:
+                    max_length = len(substring)
+                    longest_substring = substring
+        
+        return longest_substring
