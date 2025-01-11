e2902a60368e8e6023dceba91ee53d2bdc0ef6a0
Dagmawi Hailu <135310799+Dagmawi-Y@users.noreply.github.com>
2025-01-11 23:52:09 +0300

    Time: 30 ms (78.85%), Space: 18.3 MB (9.32%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        if k > len(s):
+            return False
+        char_count = Counter(s)
+        
+        odd_chars = sum(1 for count in char_count.values() if count % 2 != 0)
+        
+        return odd_chars <= k
